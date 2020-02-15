94ca87b75156f39e66a9416b0ff8f840ddb14a7a
Islam Attrash <isattrash@gmail.com>
2020-02-15 12:10:47 +0200

    seo tags

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Head from 'next/head';
 import Header from '../components/Header';
 import DocumentationGenerator from '../components/DocumentationGenerator';
 import './globalStyles.scss';
 
 export default function App() {
   return (
-    <div className="rudash-website">
-      <Header/>
-      <DocumentationGenerator/>
-    </div>
+    <>
+      <Head>
+          <title>Rudash - Lodash for Ruby Apps</title>
+          <meta name="robots" content="index, follow"/>
+          <meta name="description" content="Rudash - The Lodash popular JavaScript library exported as a Gem for Ruby developers"/>
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="Rudash Website" />
+      </Head>
+      <div className="rudash-website">
+        <Header/>
+        <DocumentationGenerator/>
+      </div>
+    </>
   );
 };
